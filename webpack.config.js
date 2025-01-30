f1acd8fdbc633b7ff8f3ea029a509b8d3f1dea9c
FL0WL0W <dhjohnson5@gmail.com>
2025-01-29 18:09:56 -0600

    remove docs folders

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = (env, argv) => { return {
     },
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        path: path.resolve(__dirname, 'docs'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
         clean: true,
     },
@@ -29,7 +29,7 @@ module.exports = (env, argv) => { return {
     },
     devServer: {
         static: {
-            directory: path.resolve(__dirname, "docs")
+            directory: path.resolve(__dirname, "dist")
         },
         proxy: [
             {
